0972f0c6ca67e5bc82530b8d4cd85bcf432fa523
ChuYu Wang <stardust2021@163.com>
2022-12-17 19:12:11 +0800

    remove current mirror mos from differential pair list

--- Differential_pair.hs
+++ Differential_pair.hs
@@ -15,8 +15,17 @@ module Differential_pair where
 import Data_define
 import Basic_function
 
-is_differential_pair :: Device -> Device -> Bool
-is_differential_pair inst1 inst2 =
+device_in_mosgroup_list :: Device -> [MosGroup] -> Bool
+device_in_mosgroup_list inst [] = False
+device_in_mosgroup_list inst (group:groupList) = 
+    string_eq (inst_name inst, diode_name group) ||
+    string_eq (inst_name inst, mir_name group) ||
+    device_in_mosgroup_list inst groupList
+
+is_differential_pair :: Device -> Device -> [MosGroup] -> Bool
+is_differential_pair inst1 inst2 mirlist =
+    (not (device_in_mosgroup_list inst1 mirlist)) &&
+    (not (device_in_mosgroup_list inst2 mirlist)) &&
     (inst_type inst1 == inst_type inst2) &&
     (snd (source_pin inst1) == snd (source_pin inst2)) &&
     (snd (bulk_pin inst1) == snd (bulk_pin inst2)) &&
@@ -30,11 +39,11 @@ is_differential_pair inst1 inst2 =
     (snd (drain_pin inst1) /= snd (drain_pin inst2)) &&
     (snd (gate_pin inst1) /= snd (gate_pin inst2))
 
-differential_pair_list :: [Device] -> [(Device, Device)]
-differential_pair_list [] = []
-differential_pair_list (inst:instlist) 
-    = [(inst, anotherinst) | anotherinst <- instlist, is_differential_pair inst anotherinst] ++ differential_pair_list instlist
+differential_pair_list :: [Device] -> [MosGroup] -> [(Device, Device)]
+differential_pair_list [] mirlist = []
+differential_pair_list (inst:instlist) mirlist=
+    [(inst, anotherinst) | anotherinst <- instlist, is_differential_pair inst anotherinst mirlist] ++ differential_pair_list instlist mirlist
 
-differential_pair_extract :: [Device] -> [MosGroup]
-differential_pair_extract devices
-    = map gen_mosgroup (differential_pair_list devices)
+differential_pair_extract :: [Device] -> [MosGroup] -> [MosGroup]
+differential_pair_extract devices mirlist = 
+    map gen_mosgroup (differential_pair_list devices mirlist)
--- main.hs
+++ main.hs
@@ -10,11 +10,11 @@ main = do
     --putStrLn "Please enter Netlist File Path : "
     --filepath <- getLine
     --linelist <- read_device filepath
-    linelist <- read_device "./netlist"
+    linelist <- read_device "./ref/netlist"
     let devices = set_devices linelist
     --show_devices devices
     let mirlist = current_mirror_extract devices
-    let diflist = differential_pair_extract devices
-    write_result "./current_mirror.txt" mirlist
-    write_result "./differential_pair.txt" diflist
+    let diflist = differential_pair_extract devices mirlist
+    write_result "./ref/current_mirror.txt" mirlist
+    write_result "./ref/differential_pair.txt" diflist
     return mirlist
